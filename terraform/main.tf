77a2efeab7d55aa6a571951df15dc43ce4ea91c8
Josh <jsdev305@gmail.com>
2026-06-02 21:14:50 -0400

    Add AWS provider and provision S3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,30 @@
 terraform {
- required_version = ">= 1.0"
+  required_version = ">= 1.0"
 
- required_providers {
-   local = {
-     source = "hashicorp/local"
-     version = "~> 2.5"
-   }
- }
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
- 
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 
 resource "local_file" "hello" {
   filename = "hello.txt"
-  content = "This is an example message text"
- }
+  content  = "This is an example message text"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "jsdev305-aws-serverless-pipeline-demo"
+}
 
