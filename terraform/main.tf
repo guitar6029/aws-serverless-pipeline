fd1fd8e38adc0443b8081dcdadc025890dc2141b
Josh <jsdev305@gmail.com>
2026-06-03 14:18:07 -0400

    Add S3 versioning and encryption

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,22 @@ resource "aws_s3_bucket" "demo" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
