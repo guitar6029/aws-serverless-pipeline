66df6e3afdcbddcf9d3044d274b6f4ae3da3530b
Josh <jsdev305@gmail.com>
2026-06-06 13:21:19 -0400

    Add path /payments to the Payments API

--- terraform/api.tf
+++ terraform/api.tf
@@ -4,7 +4,15 @@
 #   protocol_type              = "HTTP"
 # }
 
-#for learning purposes pick v1
+# for learning purposes pick v1
 resource "aws_api_gateway_rest_api" "payments" {
- name = "payments-api"
+  name = "payments-api"
+}
+
+# for payments root
+# resource "<resource_type>" "<local_name>"
+resource "aws_api_gateway_resource" "payments" {
+  rest_api_id = aws_api_gateway_rest_api.payments.id
+  parent_id   = aws_api_gateway_rest_api.payments.root_resource_id
+  path_part   = "payments"
 }
