3d03143eff8bc9ee03248c499a791b284eccff92
Josh <jsdev305@gmail.com>
2026-06-06 21:43:38 -0400

    Add integration for payment api

--- terraform/api.tf
+++ terraform/api.tf
@@ -35,3 +35,13 @@ resource "aws_api_gateway_method" "get_payment" {
 
   authorization = "NONE"
 }
+
+
+resource "aws_api_gateway_integration" "payments_api" {
+  rest_api_id             = aws_api_gateway_rest_api.payments.id
+  resource_id             = aws_api_gateway_resource.payment_id.id
+  http_method             = aws_api_gateway_method.get_payment.http_method
+  uri                     = aws_lambda_function.payments_api.invoke_arn
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+}
