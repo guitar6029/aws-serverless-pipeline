69b078f2bd0318e12ece353e7d95b068df6ef615
Josh <jsdev305@gmail.com>
2026-06-15 15:23:24 -0400

    feat(reports): add reporting lambda infrastructure

--- terraform/api.tf
+++ terraform/api.tf
@@ -17,6 +17,14 @@ resource "aws_api_gateway_resource" "payments" {
   path_part   = "payments"
 }
 
+#for reports
+# resource "aws_api_gateway_resource" "reports" {
+#   rest_api_id = aws_api_gateway_rest_api.payments.id
+#   parent_id   = aws_api_gateway_resource.payments.id
+#   path_part   = "reports" 
+# }
+
+
 #for individual payment
 resource "aws_api_gateway_resource" "payment_id" {
   rest_api_id = aws_api_gateway_rest_api.payments.id
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -99,6 +99,30 @@ resource "aws_iam_role_policy" "lambda_dynamodb_write" {
   })
 }
 
+# REPORTS API ROLE
+resource "aws_iam_role" "reports_api_role" {
+  name = "reports-api-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "reports_api_basic_execution" {
+  role       = aws_iam_role.reports_api_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# END REPORTS
+
 # PAYMENT APi ROLE
 
 resource "aws_iam_role" "payments_api_role" {
