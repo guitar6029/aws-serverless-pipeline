1ca04c6774d3468d84ee84fc515d8ea2e2547f37
Josh <jsdev305@gmail.com>
2026-06-30 13:50:05 -0400

    updated Drone createDrone method

--- learning/java-spring/spring-playground/src/main/java/com/joshdev/telemetrydemo/service/DroneService.java
+++ learning/java-spring/spring-playground/src/main/java/com/joshdev/telemetrydemo/service/DroneService.java
@@ -1,12 +1,27 @@
 package com.joshdev.telemetrydemo.service;
 
-import com.joshdev.telemetrydemo.model.Drone;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+
+import com.joshdev.telemetrydemo.dto.drone.CreateDroneRequest;
+import com.joshdev.telemetrydemo.model.Drone;
+import com.joshdev.telemetrydemo.model.DroneStatus;
 
 @Service
 public class DroneService {
 
     public Drone getDrone(String id) {
-        return new Drone(id, "ONLINE", 94, 42.5);
+        return new Drone(id, "example", DroneStatus.ONLINE, 94, 42.5);
+    }
+
+    public Drone createDrone(CreateDroneRequest request) {
+        return new Drone(
+                UUID.randomUUID().toString(),
+                request.getName(),
+                DroneStatus.OFFLINE,
+                request.getBattery(),
+                25.0
+        );
     }
 }
