1219853059857442a0b0087ae4c513c5aef2f74c
Josh <jsdev305@gmail.com>
2026-06-29 20:53:19 -0400

    add search get function

--- learning/java-spring/spring-playground/src/main/java/com/joshdev/telemetrydemo/controller/DroneController.java
+++ learning/java-spring/spring-playground/src/main/java/com/joshdev/telemetrydemo/controller/DroneController.java
@@ -3,6 +3,7 @@ package com.joshdev.telemetrydemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joshdev.telemetrydemo.model.Drone;
@@ -44,6 +45,14 @@ public class DroneController {
     @GetMapping("/ping")
     public String getPing() {
         return emailClient.ping();
+    }
+
+    @GetMapping("/search")
+    public String searchDrones(
+            @RequestParam String status,
+            @RequestParam int page
+    ) {
+        return "status=%s page%d".formatted(status, page);
     }
 
 }
