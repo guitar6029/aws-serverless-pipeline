dc7af7490b1147a73f9b06966e76bee17b8117bd
Josh <jsdev305@gmail.com>
2026-07-01 11:07:56 -0400

    updated DroneRepository, the id is UUID now

--- learning/java-spring/spring-playground/src/main/java/com/joshdev/telemetrydemo/repository/DroneRepository.java
+++ learning/java-spring/spring-playground/src/main/java/com/joshdev/telemetrydemo/repository/DroneRepository.java
@@ -1,9 +1,11 @@
 package com.joshdev.telemetrydemo.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.joshdev.telemetrydemo.model.Drone;
 
-public interface DroneRepository extends JpaRepository<Drone, String> {
+public interface DroneRepository extends JpaRepository<Drone, UUID> {
 
 }
